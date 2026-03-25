6244c4581ddd0c1b6c937e93b14104b2f5a8997f
santamaria000 <shomacco@gmail.com>
2026-03-26 03:34:25 +0900

    変更内容を一言

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
      Config
   ========================================================= */
   const CONFIG = {
-    endpoint: "https://party-entrance-api.ukiyofilm.workers.dev/api/entry",
+    endpoint: "/api/entry",
     method: "POST",
     redirectUrl: "message.html",
     submitTimeout: 15000,
